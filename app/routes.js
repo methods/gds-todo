b68e238bf05dad06a6aca5e71e48350e2e8854e6
Alex <alja.platt@gmail.com>
2022-06-25 13:46:16 +0100

    added fix-date branch

--- app/routes.js
+++ app/routes.js
@@ -2,13 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 // Add your routes here - above the module.exports line
-let dateObj;
+// let dateObj;
 
-router.post("/date-answers", (req, res) => {
-//   dateObj = req.session.data['date-input'];
-  
-  console.log(req.session.data);
-  res.redirect('/steps')
+// router.post("/date-answers", (req, res) => {
+// //   dateObj = req.session.data['date-input'];
+// let data = req.session.data["date-input"];
+//   console.log(req.session.data);
+//   res.redirect('/steps')
+// });
+
+router.post("/steps", (req, res) => {
+  // let {day, month, year} = req.body;
+  let date = req.body
+  console.log(typeof date, date);
+  let {date-input-day, date-input-month, date-input-year } = date
+  // return res.send()
+  /**
+   * let {date-input-day, date-input-month, date-input-year } = date
+   * 'date-input-day': '2',
+  'date-input-month': '3',
+  'date-input-year': '6'
+   */
 });
 
 module.exports = router;
