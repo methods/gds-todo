bc7a5602e1b650b9aeb96c35434a234ffe110972
Alex <alja.platt@gmail.com>
2022-06-27 15:00:40 +0100

    date validation

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const bodyParser = require('body-parser')
+
+// router.use(bodyParser.urlencoded({extended: false}))
 
 router.post("/date-answer", function (req, res) {
 	// const toDoDate = req.session.data["date-day"];
-
-	console.log(req.body);
+const object = req.body
+	// console.log(req.body[0]);
+    const day = object['date-day']
+    const month = object['date-month']
+    const year = object['date-year']
+	console.log(day, month, year);
 	res.redirect("/steps");
 });
 
