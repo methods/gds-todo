56d046454c9036f592f763d4421ba3eab1852d6e
Hajara Iyal <hajaraiyal@gmail.com>
2022-06-27 14:16:51 +0100

    date error page added

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+router.post("/date-answer", function (req, res) {
+	const toDoDate = req.session.data["date-day"];
+
+	console.log(toDoDate);
+	res.redirect("/steps");
+});
+
 module.exports = router;
