2bf18129f913ee504c7ac10906bcb3c0653ee486
Alex <alja.platt@gmail.com>
2022-06-27 15:55:49 +0100

    date err handling

--- app/routes.js
+++ app/routes.js
@@ -5,33 +5,26 @@ const bodyParser = require("body-parser");
 // router.use(bodyParser.urlencoded({extended: false}))
 
 router.post("/date-answer", function (req, res) {
-	// const toDoDate = req.session.data["date-day"];
-	const object = req.body;
-	// console.log(req.body[0]);
-	const day = object["date-day"];
-	const month = object["date-month"];
-	const year = object["date-year"];
-	const givenDate = `${day}-${month}-${year}`;
-	givenDate = new Date(givenDate);
-	const currentDate = new Date();
+  // const toDoDate = req.session.data["date-day"];
+  const object = req.body;
+  // console.log(req.body);
+  const day = object["date-day"];
+  const month = object["date-month"];
+  const year = object["date-year"];
+  let givenDate = `${day}-${month}-${year}`;
+  console.log(givenDate);
+  givenDate = new Date(givenDate);
+  const currentDate = new Date();
 
-	if (givenDate > currentDate) {
-		alert("Given date is in the future");
-	} else {
-		alert("Given date is in the past");
-	}
+  if (givenDate > currentDate) {
+    console.log("Given date is in the future");
+    
+  } else {
+    console.log("Given date is in the past");
+    res.redirect('/steps')
+  }
 
-	// res.redirect("/steps");
 });
 
 module.exports = router;
 
-// var GivenDate = "2018-02-22";
-// var CurrentDate = new Date();
-// GivenDate = new Date(GivenDate);
-
-// if (GivenDate > CurrentDate) {
-// 	alert("Given date is greater than the current date.");
-// } else {
-// 	alert("Given date is not greater than the current date.");
-// }
