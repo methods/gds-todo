284475675b67bc4fd6006f982246325d48afbf6b
Alex <alja.platt@gmail.com>
2022-06-28 14:12:43 +0100

    page not found page

--- app/routes.js
+++ app/routes.js
@@ -22,18 +22,49 @@ router.post("/date-answer", function (req, res) {
 	}
 });
 
-// 404 error page handling
+
+
 router.get("/", function (req, res) {
+	res.render("index");
+});
+
+// 404 error page handling
+router.get("/wellbeing", function (req, res) {
 	res.render("wellbeing");
+});
+
+router.get("/date", function (req, res) {
+	res.render("date");
+});
+router.get("/date-err", function (req, res) {
+	res.render("date-err");
 });
 
 router.get("/steps", function (req, res) {
-	res.render("wellbeing");
+	res.render("steps");
+});
+
+router.get("/water-intake", function (req, res) {
+	res.render("water-intake");
+});
+
+router.get("/5-a-day", function (req, res) {
+	res.render("5-a-day");
+});
+
+router.get("/check-answers", function (req, res) {
+	res.render("check-answers");
+});
+router.get("/confirmation-page", function (req, res) {
+	res.render("confirmation-page");
 });
 
 //The 404 Route (ALWAYS Keep this as the last route)
-router.get("*", function (req, res) {
-	res.render("404");
+router.get(function (req, res) {
+	res.status(404).render("404");
 });
+// router.use(function (req, res) {
+// 	res.status(404).render("404");
+// });
 
 module.exports = router;
