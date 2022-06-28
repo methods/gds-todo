e4448861c3be04332ebcc6744a24c2aa0b00abb4
Hajara Iyal <hajaraiyal@gmail.com>
2022-06-28 18:01:31 +0100

    exported functions from controllers and into routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,17 @@
 const express = require("express");
-const isDateValid = require("./controllers/index");
 const router = express.Router();
-// const app = express();
+
+const isDateValid = require("./controllers/index");
+const homeView = require("./controllers");
+const wellbeingView = require("./controllers");
+const dateView = require("./controllers");
+const dateErrorView = require("./controllers");
+const stepsView = require("./controllers");
+const waterIntakeView = require("./controllers");
+const fiveAdayView = require("./controllers");
+const checkAnswersView = require("./controllers");
+const confirmationView = require("./controllers");
+const errorPageView = require("./controllers");
 
 // Date validation
 router.post("/date-answer", function (req, res) {
@@ -10,47 +20,24 @@ router.post("/date-answer", function (req, res) {
 	res.redirect(url);
 });
 
-router.get("/todo", function (req, res) {
-	res.render("index");
-});
+router.get("/todo", homeView);
 
-// 404 error page handling
-router.get("/todo/wellbeing-todo", function (req, res) {
-	res.render("wellbeing");
-});
+router.get("/todo/wellbeing-todo", wellbeingView);
 
-router.get("/todo/wellbeing-todo/date", function (req, res) {
-	res.render("date");
-});
-router.get("/todo/wellbeing-todo/date-err", function (req, res) {
-	res.render("date-err");
-});
+router.get("/todo/wellbeing-todo/date", dateView);
 
-router.get("/todo/wellbeing-todo/steps", function (req, res) {
-	res.render("steps");
-});
+router.get("/todo/wellbeing-todo/date-err", dateErrorView);
 
-router.get("/todo/wellbeing-todo/water-intake", function (req, res) {
-	res.render("water-intake");
-});
+router.get("/todo/wellbeing-todo/steps", stepsView);
 
-router.get("/todo/wellbeing-todo/5-a-day", function (req, res) {
-	res.render("5-a-day");
-});
+router.get("/todo/wellbeing-todo/water-intake", waterIntakeView);
 
-router.get("/todo/wellbeing-todo/check-answers", function (req, res) {
-	res.render("check-answers");
-});
-router.get("/todo/wellbeing-todo/confirmation", function (req, res) {
-	res.render("confirmation");
-});
+router.get("/todo/wellbeing-todo/5-a-day", fiveAdayView);
 
-//The 404 Route (ALWAYS Keep this as the last route)
-router.get(function (req, res) {
-	res.status(404).render("404");
-});
-// router.use(function (req, res) {
-// 	res.status(404).render("404");
-// });
+router.get("/todo/wellbeing-todo/check-answers", checkAnswersView);
+
+router.get("/todo/wellbeing-todo/confirmation", confirmationView);
+
+router.get("/todo/wellbeing-todo/404", errorPageView);
 
 module.exports = router;
