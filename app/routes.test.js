85c96fc36140c29eefcebc8ebb46921b8fd9c3eb
Hajara Iyal <hajaraiyal@gmail.com>
2022-06-29 14:11:02 +0100

    test branch setup

---
+++ app/routes.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest");
+const express = require("express");
+const index = require("./routes");
+// const isDateValid = require("./controllers/index")
+
+// describe("GET /", function(){
+//     it("should return steps pafe if date is not in the future", async()=>{
+//         // ACT
+//         const actual = await isDateValid();
+//         request(index)
+//     })
+// })
