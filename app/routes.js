1d5d65b8d1f667be7bb892cc09fa2fa67ef712ff
Alex <alja.platt@gmail.com>
2022-06-29 15:55:16 +0100

    routes

--- app/routes.js
+++ app/routes.js
@@ -1,82 +1,76 @@
 const express = require("express");
 const router = express.Router();
-const app = express();
+// const app = express();
 
 const isDateValid = require("./controllers/index");
-const homeView = require("./controllers");
-const wellbeingView = require("./controllers/index.js");
-const dateView = require("./controllers");
-const dateErrorView = require("./controllers");
-const stepsView = require("./controllers");
-const waterIntakeView = require("./controllers");
-const fiveAdayView = require("./controllers");
-const checkAnswersView = require("./controllers");
-const confirmationView = require("./controllers");
-const errorPageView = require("./controllers");
-
 // router.use(wellbeingView)
 
-// Date validation
-// router.post("/todo/wellbeing-todo/date-err", function (req, res) {
-// 	const object = req.body;
-// 	const url = isDateValid(object);
-// 	res.redirect(url);
-// });
-router.post("/date-answer", function (req, res) {
-	const object = req.body;
-	const url = isDateValid(object);
-	res.redirect(url);
+router.post("/todo/wellbeing-todo/steps", function (req, res) {
+  const object = req.body;
+  const endpoint = isDateValid(object);
+  res.redirect(endpoint);
 });
 
 // router.get("/todo", homeView);
-app.get("/", (req, res) => {
-	res.status(200);
+router.get("/", (req, res) => {
+  res.status(200).redirect("index");
 });
 
 // router.get("/todo/wellbeing-todo", wellbeingView);
 router.get("/todo/wellbeing-todo", function (req, res) {
-	res.render("wellbeing");
+  res.render("wellbeing");
 });
 
 // router.get("/todo/wellbeing-todo/date", dateView);
 router.get("/todo/wellbeing-todo/date", function (req, res) {
-	res.render("date");
+  res.render("date");
 });
 
 // router.get("/todo/wellbeing-todo/date-err", dateErrorView);
 router.get("/todo/wellbeing-todo/date-err", function (req, res) {
-	res.render("date-err");
+  res.render("date-err");
 });
 
 // router.get("/todo/wellbeing-todo/steps", stepsView);
 router.get("/todo/wellbeing-todo/steps", function (req, res) {
-	res.render("steps");
+  res.render("steps");
 });
 
 // router.get("/todo/wellbeing-todo/water-intake", waterIntakeView);
 router.get("/todo/wellbeing-todo/water-intake", function (req, res) {
-	res.render("water-intake");
+  res.render("water-intake");
 });
 
 // router.get("/todo/wellbeing-todo/5-a-day", fiveAdayView);
 router.get("/todo/wellbeing-todo/5-a-day", function (req, res) {
-	res.render("5-a-day");
+  res.render("5-a-day");
 });
 
 // router.get("/todo/wellbeing-todo/check-answers", checkAnswersView);
 router.get("/todo/wellbeing-todo/check-answers", function (req, res) {
-	res.render("check-answers");
+  res.render("check-answers");
 });
 
 // router.get("/todo/wellbeing-todo/confirmation", confirmationView);
 router.get("/todo/wellbeing-todo/confirmation", function (req, res) {
-	res.render("confirmation");
+  res.render("confirmation");
 });
 
 // router.get("/todo/wellbeing-todo/404", errorPageView);
 router.get("/todo/wellbeing-todo/404", function (req, res) {
-	res.render("404");
+  res.render("404");
 });
 
 module.exports = router;
-module.exports = app;
+// module.exports = app;
+
+// const homeView = require("./controllers");
+// const wellbeingView = require("./controllers/index.js");
+// const dateView = require("./controllers");
+// const dateErrorView = require("./controllers");
+// const stepsView = require("./controllers");
+// const waterIntakeView = require("./controllers");
+// const fiveAdayView = require("./controllers");
+// const checkAnswersView = require("./controllers");
+// const confirmationView = require("./controllers");
+// const errorPageView = require("./controllers");
